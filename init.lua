a986e627a568f44701c50906392e1bb51455f184
iurii <hello@jurip.xyz>
2024-09-22 18:53:49 +0300

    upd

--- init.lua
+++ init.lua
@@ -39,7 +39,7 @@ if vim.fn.filereadable(project_config) == 1 then
   dofile(project_config)
 end
 
-vim.cmd [[colorscheme lackluster-hack]]
+vim.cmd [[colorscheme lackluster-mint]]
 require 'config.mappings'
 
 if vim.fn.has 'win32' == 1 or vim.fn.has 'win64' == 1 then
